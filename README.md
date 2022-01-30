c79a87a7402e5763534089c07759e600505efffe
jchanes04 <coolwolfs8@gmail.com>
2022-01-30 15:42:46 -0500

    Added ctrl/shift modifiers and selections

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
 - Cursor movement controls
 - Accessibility
 - Mobile/cross-browser testing
+- Copy/cut/paste
 
 ## Planned Settings
 
@@ -16,3 +17,10 @@
 - Normal/subscript/superscript text component
 - Symbol dictionary (what to convert to what, what to automatically super/subscript, etc.)
 - Selection color
+
+## Cursor Selection Notes
+
+1. Place cursor at beginning point (already happens)
+2. Place invisible spans one character before and after the cursor
+3. Watch for mousemove and use mouse position + getClientRect to determine which characters the cursor is most nearly between
+4. When the selection position changes, move spans to the left and right of new selection end position, and repeat until mouseup
--- src/lib/ChemicalFormulaInput.svelte
+++ src/lib/ChemicalFormulaInput.svelte
@@ -11,32 +11,33 @@
     let blinkTimeout
 
     let content = ""
-    $: leftContent = selectionStartPosition
+    $: leftContent = selectionStartPosition !== null
         ? content.slice(0, Math.min(selectionStartPosition, cursorPosition))
         : content.slice(0, cursorPosition)
-    $: rightContent = selectionStartPosition
+    $: rightContent = selectionStartPosition !== null
         ? content.slice(Math.max(selectionStartPosition, cursorPosition))
         : content.slice(cursorPosition)
-    $: selectedContent = selectionStartPosition
+    $: selectedContent = selectionStartPosition !== null
         ? content.slice(Math.min(selectionStartPosition, cursorPosition), Math.max(selectionStartPosition, cursorPosition))
         : null
     let textareaValue = "____"
 
     async function resetTextarea() {
-        const contentWordLength = content.split(/\s/).length
         setTimeout(async () => {
-            // textareaValue = contentWordLength
-            //     ? "_ ".repeat(contentWordLength).slice(0, -1)
-            //     : "____"
             textareaValue = "____"
-            // textareaElement.setSelectionRange(2 * contentWordLength, 2 * contentWordLength)
             await tick()
-            console.log('a')
             textareaElement.setSelectionRange(4, 4)
         })
     }
 
+    function deselectText() {
+        cursorPosition = Math.max(selectionStartPosition, cursorPosition)
+        selectionStartPosition = null
+    }
+
     async function switchFocus() {
+        selectionStartPosition = null
+
         setTimeout(() => {
             const selection = document.getSelection()
             if (selection.anchorOffset === selection.focusOffset) {
@@ -56,10 +57,15 @@
     }
 
     const handleInput = (async function(e: InputEvent) {
-        console.log(e)
         if (e.inputType === "deleteContentBackward") {
-            content = content.slice(0, cursorPosition - 1) + content.slice(cursorPosition)
-            if (cursorPosition > 0) cursorPosition--
+            if (selectedContent) {
+                content = leftContent + rightContent
+                cursorPosition = Math.min(cursorPosition, selectionStartPosition)
+                selectionStartPosition = null
+            } else {
+                content = content.slice(0, cursorPosition - 1) + content.slice(cursorPosition)
+                if (cursorPosition > 0) cursorPosition--
+            }
         } else if (e.inputType === "deleteWordBackward") {
             const words = leftContent.slice(0, -1).matchAll(/([a-zA-Z0-9:_']*[^a-zA-Z0-9:_']*)/g)
             let matches = []
@@ -74,8 +80,14 @@
                 ? match.value.at(-1).length
                 : 0
         } else {
-            content = content.slice(0, cursorPosition) + e.data + content.slice(cursorPosition)
-            if (cursorPosition <= content.length) cursorPosition++
+            if (selectedContent) {
+                content = leftContent + e.data + rightContent
+                cursorPosition = Math.min(selectionStartPosition, cursorPosition) + 1
+                selectionStartPosition = null
+            } else {
+                content = content.slice(0, cursorPosition) + e.data + content.slice(cursorPosition)
+                if (cursorPosition <= content.length) cursorPosition++
+            }
         }
 
         setTimeout(() => {
@@ -87,8 +99,12 @@
 
     async function handleKeydown(e: KeyboardEvent) {
         if (e.key === "ArrowLeft") {
-            if (e.ctrlKey && cursorPosition > 0) {
-                const words = leftContent.slice(0, -1).matchAll(/([a-zA-Z0-9:_']*[^a-zA-Z0-9:_']*)/g)
+            if (e.ctrlKey && e.shiftKey && cursorPosition > 0) {
+                const contentLeftOfCursor = (selectionStartPosition && selectionStartPosition < cursorPosition)
+                    ? leftContent + selectedContent
+                    : leftContent
+                
+                const words = contentLeftOfCursor.matchAll(/([a-zA-Z0-9:_']*[^a-zA-Z0-9:_']*)/g)
                 let matches = []
                 let lastMatch
                 do {
@@ -96,19 +112,66 @@
                     matches.push(lastMatch)
                 } while(!lastMatch.done)
                 const match: RegExpMatchArray = matches.length >= 3 ? matches.at(-3).value : ['', '']
-                cursorPosition -= match.at(-1).length + 1
-            } else if (cursorPosition > 0) {
+                
+                if (selectionStartPosition === null) selectionStartPosition = cursorPosition
+                cursorPosition -= match.at(-1).length
+            } else if (e.shiftKey && cursorPosition > 0) {
+                if (selectionStartPosition === null) selectionStartPosition = cursorPosition
                 cursorPosition--
+            } else if (e.ctrlKey && cursorPosition > 0) {
+                if (selectionStartPosition !== null) {
+                    cursorPosition = Math.min(selectionStartPosition, cursorPosition)
+                    selectionStartPosition = null
+                }
+                
+                const words = leftContent.matchAll(/([a-zA-Z0-9:_']*[^a-zA-Z0-9:_']*)/g)
+                let matches = []
+                let lastMatch
+                do {
+                    lastMatch = words.next()
+                    matches.push(lastMatch)
+                } while(!lastMatch.done)
+                const match: RegExpMatchArray = matches.length >= 3 ? matches.at(-3).value : ['', '']
+                cursorPosition -= match.at(-1).length
+            } else {
+                if (selectionStartPosition !== null) {
+                    cursorPosition = Math.min(selectionStartPosition, cursorPosition)
+                    selectionStartPosition = null
+                } else if (cursorPosition > 0) {
+                    cursorPosition--
+                }
             }
             resetTextarea()
             restartBlink()
         } else if (e.key === "ArrowRight") {
-            if (e.ctrlKey && cursorPosition <= content.length) {
+            if (e.ctrlKey && e.shiftKey && cursorPosition <= content.length) {
+                const contentRightOfCursor = (selectionStartPosition && selectionStartPosition > cursorPosition)
+                    ? selectedContent + rightContent
+                    : rightContent
+                const words = contentRightOfCursor.matchAll(/([^a-zA-Z0-9:_']?[a-zA-Z0-9:_']+|[^a-zA-Z0-9:_']+)?/g)
+                const match = words.next().value
+                
+                if (selectionStartPosition === null) selectionStartPosition = cursorPosition
+                cursorPosition += match.at(1)?.length ?? 0
+            } else if (e.shiftKey && cursorPosition <= content.length) {
+                if (selectionStartPosition === null) selectionStartPosition = cursorPosition
+                cursorPosition++
+            } else if (e.ctrlKey && cursorPosition <= content.length) {
+                if (selectionStartPosition !== null) {
+                    cursorPosition = Math.max(selectionStartPosition, cursorPosition)
+                    selectionStartPosition = null
+                }
+
                 const words = rightContent.slice(1).matchAll(/([a-zA-Z0-9:_']+|[^a-zA-Z0-9:_']+)?/g)
                 const match = words.next().value
-                cursorPosition += match.at(1).length + 1
+                cursorPosition += (match.at(1)?.length ?? 0) + 1
             } else if (cursorPosition <= content.length) {
-                cursorPosition++
+                if (selectionStartPosition !== null) {
+                    cursorPosition = Math.max(selectionStartPosition, cursorPosition)
+                    selectionStartPosition = null
+                } else {
+                    cursorPosition++
+                }
             }
             resetTextarea()
             restartBlink()
@@ -117,16 +180,17 @@
 
     async function restartBlink() {
         clearTimeout(blinkTimeout)
-        cursorElement.classList.remove('blinking')
+        if (cursorElement) cursorElement.classList.remove('blinking')
+        
         blinkTimeout = setTimeout(() => {
-            cursorElement.classList.add('blinking')
+            if (cursorElement) cursorElement.classList.add('blinking')
         }, 50)
     }
 </script>
 
 <div class="chemical-formula-input">
     <textarea tabindex={-1} autocapitalize="off" autocomplete="off" autocorrect="off" spellcheck="false"
-        bind:this={textareaElement} on:input={handleInput} on:keydown={handleKeydown} bind:value={textareaValue} />
+        bind:this={textareaElement} on:input={handleInput} on:keydown={handleKeydown} on:blur={deselectText} bind:value={textareaValue} />
     <div class="input-box" on:focus={switchFocus} bind:this={inputElement} contenteditable>
         {#if selectedContent}
             <span class="left">{leftContent}</span><span class="selection">{selectedContent}</span><span class="right">{rightContent}</span>
